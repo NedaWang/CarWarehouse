e708ac90abe198b4cd76f6fcb7e010c44bb82384
NedaWang <NedaWang@users.noreply.github.com>
2021-04-14 23:56:18 +1000

    view binding test

--- app/src/main/java/com/neda/carwarehouse/MainActivity.java
+++ app/src/main/java/com/neda/carwarehouse/MainActivity.java
@@ -1,14 +1,39 @@
 package com.neda.carwarehouse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.neda.carwarehouse.databinding.CarInfoFormBinding;
+import com.neda.carwarehouse.databinding.CoordinatorLayoutBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private @NonNull CoordinatorLayoutBinding binding;
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.coordinator_layout);
+        binding = CoordinatorLayoutBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+
+        fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Hello", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
